3dd5181415776431fee474e693d35937e7dd2ef9
Deepanshu bharagav <132800215+Deepanshubhargav1@users.noreply.github.com>
2024-12-08 11:21:15 +0530

    Add files via upload

---
+++ codealpha/Hangman game/words.py
@@ -0,0 +1,38 @@
+# Words for hangman game
+
+words = (
+    # Citrus Fruits
+    "Orange", "Lemon", "Lime", "Grapefruit", "Tangerine", "Pomelo", "Clementine",
+
+    # Berries
+    "Strawberry", "Raspberry", "Blueberry", "Blackberry", "Cranberry", "Gooseberry",
+    "Elderberry", "Mulberry",
+
+    # Tropical Fruits
+    "Banana", "Mango", "Pineapple", "Papaya", "Kiwi", "Guava", "Lychee",
+    "Passion Fruit", "Durian", "Jackfruit", "Mangosteen",
+
+    # Stone Fruits
+    "Peach", "Plum", "Cherry", "Apricot", "Nectarine", "Date",
+
+    # Pome Fruits
+    "Apple", "Pear", "Quince",
+
+    # Melons
+    "Watermelon", "Cantaloupe", "Honeydew",
+
+    # Exotic Fruits
+    "Dragon Fruit", "Starfruit", "Rambutan", "Longan", "Snake Fruit", "Buddha's Hand",
+
+    # Vine Fruits
+    "Grapes", "Passionfruit", "Kiwifruit",
+
+    # Nut-Like Fruits
+    "Coconut", "Avocado", "Olive",
+
+    # Aggregate Fruits
+    "Fig", "Pomegranate",
+
+    # Other Fruits
+    "Persimmon", "Tamarind", "Jujube"
+)
